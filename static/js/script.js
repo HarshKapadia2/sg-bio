1338326ff10dd311780f9c95f97dbb1a95245684
Harsh Kapadia <harshgkapadia@gmail.com>
2021-01-20 22:32:58 +0530

    :sparkles: feat: Find dominant color from uploaded avatar and output in console

--- static/js/script.js
+++ static/js/script.js
@@ -3,13 +3,54 @@ const avatarInput = document.querySelector("#avatar-input");
 
 avatarUpload.addEventListener("click", () => avatarInput.click());
 
-avatarInput.addEventListener("change", () => getBase64(avatarInput.files[0]));
+avatarInput.addEventListener
+(
+	"change",
+	async () =>
+	{
+		const base64Img = await getBase64(avatarInput.files[0]);
+		console.log(base64Img);
 
-function getBase64(file)
+		const dominantColour = await getDominantColour(base64Img);
+		console.log("The dominant colour in uploaded image is RGB(" + dominantColour + ").");
+	}
+);
+
+async function getBase64(file)
 {
-	const reader = new FileReader();
+	return new Promise
+	(
+		(resolve, reject) =>
+		{
+			const reader = new FileReader();
 
-	reader.readAsDataURL(file);
-	reader.onload = () => console.log(reader.result);
-	reader.onerror = (err) => console.error(err);
+			reader.addEventListener("error", reject);
+			reader.addEventListener("load", () => resolve(reader.result));
+			reader.readAsDataURL(file);
+		}
+	);
+}
+
+async function getDominantColour(base64Str)
+{
+	return new Promise
+	(
+		(resolve, reject) =>
+		{
+			const img = document.createElement("img");
+			img.src = base64Str;
+			
+			img.addEventListener("error", reject);
+
+			img.addEventListener
+			(
+				"load",
+				() =>
+				{
+					const colorThief = new ColorThief();
+					resolve(colorThief.getColor(img));
+				}
+			);
+		}
+	);
 }
