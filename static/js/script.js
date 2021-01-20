9b0573a423a20f71d3724793ce95f5f3c4c7e1ec
Harsh Kapadia <harshgkapadia@gmail.com>
2021-01-20 21:10:30 +0530

    :sparkles: feat: Convert uploaded avatar to base64 in console

--- static/js/script.js
+++ static/js/script.js
@@ -2,3 +2,14 @@ const avatarUpload = document.querySelector("#avatar-upload-div");
 const avatarInput = document.querySelector("#avatar-input");
 
 avatarUpload.addEventListener("click", () => avatarInput.click());
+
+avatarInput.addEventListener("change", () => getBase64(avatarInput.files[0]));
+
+function getBase64(file)
+{
+	const reader = new FileReader();
+
+	reader.readAsDataURL(file);
+	reader.onload = () => console.log(reader.result);
+	reader.onerror = (err) => console.error(err);
+}
